6056585305e9b81f05b592688cbbd41825ed741f
Arielanandasafutrapratama <arielananda089@gmail.com>
2025-01-05 16:36:04 +0800

    Add files via upload

---
+++ MODUL 4/09-OprasiAssignment/main.py
@@ -0,0 +1,31 @@
+# operasi yang dapat di lakukan dengan meringkas
+# operasi ditambah dengan asisgnment
+
+a = 5 # assignment
+print("nilai a = ", a)
+a += 1 # artinya a = a + 1
+print("nilai a += 1, nilai a menjadi = ", a)
+
+a -= 1 # artinya a = a - 1
+print("nilai a -= 1, nilai a menjadi = ", a)
+
+a *= 5 # artinya a = a * 5
+print("nilai a += 5, nilai a menjadi = ", a)
+
+a /= 5 # artinya a = a / 5
+print("nilai a /= 5, nilai a menjadi = ", a)
+
+# modulus dan ploor division
+# %=, //=
+a %= 3 # artinya a = a % 3
+print("nilai a %= 5, nilai a menjadi = ", a)
+
+a //= 1 # artinya a = a // 1
+print("nilai a //= 1, nilai a menjadi = ", a)
+# pangkat / exponen 
+a **= 10 # artinya a = a ** 6
+print("nilai a **= 5, nilai a menjadi = ", a)
+
+# OR
+c |= True 
+print("nilai c = ", c)
